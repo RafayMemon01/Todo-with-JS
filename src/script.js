fb31e129392f27da178184fab64d4168cf19ceff
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-09 11:13:46 +0500

    basic styling added

--- src/script.js
+++ src/script.js
@@ -6,11 +6,13 @@ document.getElementById("form").addEventListener("submit",(e)=>{
     let userInput = document.getElementById("userInput").value;
     allTodos.push(userInput);
     console.log(allTodos);  
+    document.getElementById("allTodos").innerHTML = ""
     cardAdd()
 })
 
 const cardAdd = () =>{
     allTodos.forEach((todo)=>{
-        document.getElementById("allTodos").innerHTML = `<p>${todo}</p>`
+        
+        document.getElementById("allTodos").innerHTML += `<p class="bg-white py-2 px-10">${todo}</p>`
     })
 }
